dca4b36e637038e7198a70105e4eff85a764098e
lizh-Cloud-20180707 <891968069@qq.com>
2018-07-21 23:27:53 +0800

    代码调通了， 数据驱动现在用不上，还缺一个文件。 在test.xlsx 下添加方法及步骤即可

--- test.py
+++ test.py
@@ -1,7 +1,40 @@
 from selenium import webdriver
+import time
 import os
-driver = webdriver.Chrome("/Users/lizg2018/PycharmProjects/pyauto/webdriver/others/chromedriver")
+driver = webdriver.Chrome()
+
+
+chrome_options = webdriver.ChromeOptions()
+
+
+
+
+chrome_options.add_argument('--proxy-server=http://100.67.154.166:51201')
+
+
+
+
+driver = webdriver.Chrome(chrome_options=chrome_options)
 ''''''
-driver.get("http://www.baidu.com")
-driver.get_screenshot_as_file("/Users/lizg2018/PycharmProjects/pyauto/exceptionpictures/2018-7-21/1.png")
-print(os.name)
+#driver.get("http://www.baidu.com")
+#driver.get_screenshot_as_file("/Users/lizg2018/PycharmProjects/pyauto/exceptionpictures/2018-7-21/1.png")
+#print(os.name)
+driver.get("https://cas.env12.shuguang.com/cas/login?service=https%3A%2F%2Fmanage.env12.shuguang.com%2Fmanage%2Flogin%2Fcas")
+print("登录前："+driver.title)
+
+time.sleep(3)
+driver.find_element_by_id("username").send_keys("admin")
+
+
+# In[8]:
+
+
+driver.find_element_by_id("password").send_keys("Admin123")
+
+
+# In[9]:
+
+
+driver.find_element_by_xpath('//input[@value="登录"]').click()
+time.sleep(5)
+print("登录后"+driver.title)
